8cdae97e04360bf763651086fc1047418e5e281c
Dulapah Vibulsanti <dulapah6@gmail.com>
2021-12-23 23:02:22 +0700

    Update

---
+++ c-cpp/SDL2/1stSDLWindow/1stSDLWindow/main.cpp
@@ -0,0 +1,17 @@
+#include "Game.hpp"
+
+Game* game = nullptr;
+
+int main(int argc, const char* argv[]) {
+	game = new Game();
+
+	while (game->running()) {
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+
+	game->clean();
+
+	return 0;
+}
