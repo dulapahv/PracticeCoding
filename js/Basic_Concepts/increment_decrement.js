8b0d889d4108ff920a4ebaa9ec6e8a4f925bc2ef
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-02-09 20:28:45 +0700

    Complete basic concepts

---
+++ js/Basic_Concepts/increment_decrement.js
@@ -0,0 +1,11 @@
+/*
+Increment ++
+The increment operator increases the numeric value of its operand by 1.
+When placed before the operand, it’ll return the incremented value. When placed after it,
+it’ll return the original value and then increments the operand.
+
+Decrement --
+The decrement operator decreases the numeric value of its operand by 1.
+When placed before the operand, it’ll return the decremented value. When placed after the operand,
+it’ll return the original value and then decrements the operand.
+*/
