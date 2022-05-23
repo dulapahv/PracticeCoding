3c7510c29a958a7cfd01fd3e5f49cf21284cf274
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-05-24 02:13:55 +0700

    Update on UNIX commands

--- unix-shell/Help/Help.sh
+++ unix-shell/Help/Help.sh
@@ -1,14 +1,5 @@
-# Type man rm to read the manual for the rm command.
-man {command}
-
-# Give short description of command.
-whatis {command}
-
-# Allows regular users to run programs with the security privileges of the superuser or root
-sudo
-
-# Gives a list of all past commands typed in the current terminal session
-history
-
-# Clears the terminal
-clear
+man {command}       # Type man rm to read the manual for the rm command.
+whatis {command}    # Give short description of command.
+sudo                # Allows regular users to run programs with the security privileges of the superuser or root
+history             # Gives a list of all past commands typed in the current terminal session
+clear               # Clears the terminal
