e7bbf5c82e542cae513fdca696c3a9eefd0cfb07
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-12-14 20:52:40 +0700

    Build base file

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="font-sans text-8xl not-italic font-semibold text-center p-2 bg-blue-400 text-white border-4 border-yellow-500">
+            <h1>Hello World!</h1>
+        </div>
+    );
+};
+
+export default App;
