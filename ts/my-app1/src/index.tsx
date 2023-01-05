2f26a141624bbebee8ed55a9fdca407af9007235
Dulapah Vibulsanti <dulapah6@gmail.com>
2023-01-05 18:18:55 +0700

    update coding

--- ts/my-app1/src/index.tsx
+++ ts/my-app1/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
@@ -7,7 +8,7 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-    <React.StrictMode>
+    <BrowserRouter>
         <App />
-    </React.StrictMode>
+    </BrowserRouter>
 );
---
+++ ts/my-app1/src/pages/index.js
@@ -0,0 +1,1 @@
+export { default as GramSchmidt } from "./GramSchmidt";
